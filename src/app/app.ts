4e995090ecb442c5293c16ee2c21e60a80b74484
Amalajerlina <jerlina331@gmail.com>
2026-02-05 23:48:21 +0530

    angular project 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,43 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule,FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
- title = 'Project-1';
+  title = 'Project-2';
 
-cname = 'St Joseph College of Engineering';
-clocation = 'Sriperumbudur';
+  // Interpolation
+  public name:String = 'St Joseph College';
+
+  //Property Binding
+  public imgUrl:String = 'Flower.jpg';
+
+  //Event Binding
+  public message:String = '';
+
+  //Two way Binding
+  public location:String = 'Kancheepuram';
+
+  public invisible:boolean = false;
+
+  public course:String[] = ['Angular', 'React', 'Vue', 'NodeJS'];
+
+
+  onClick():void{
+    this.message = 'Welcome to Learn Angular';  }
+
+
+    toggleButton():void
+    {
+      this.invisible = !this.invisible
+    }
+
 
 }
